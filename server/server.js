9d54a9d82090a45c98c05b724d0878b35e61e202
abolfazl <abazsh3@gmail.com>
2019-07-17 16:46:55 +0430

    added delete

--- server/server.js
+++ server/server.js
@@ -1,22 +1,32 @@
 let express = require('express');
 let bodyParser = require('body-parser');
-let {mongoose}=require('./db/mongoose');
-let {Todo}=require('./models/Todo');
-let {User}=require('./models/User');
+let {mongoose} = require('./db/mongoose');
+let {Todo} = require('./models/Todo');
+let {User} = require('./models/User');
 
-let app =express();
+let app = express();
 app.use(bodyParser.json());
-app.post('/todos',(req,res)=>{
+
+app.post('/todos', (req, res) => {
     let todo = new Todo({
-        text:req.body.text
+        text: req.body.text
     })
-    todo.save().then((doc)=>{
+    todo.save().then((doc) => {
         res.send(doc);
-    },(e)=>{
+    }, (e) => {
         res.status(400).send(e);
     })
 });
 
-app.listen(3000,()=>{
+app.get('/todos',(req,res)=>{
+    Todo.find().then((todo)=>{
+        res.send({todo});
+    },(e)=>{
+        res.status(400).send(e)
+    });
+});
+
+app.listen(3000, () => {
     console.log('app is running on port 3000');
 });
+module.exports = {app};
